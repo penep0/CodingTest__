3e5970e47e2a26f0cf014b52268f9bf4bd0fe82d
penep0 <102152521+penep0@users.noreply.github.com>
2025-04-03 10:38:05 +0900

    Add files via upload

---
+++ codingtest/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[][] name = new String[n][2];
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            name[i][0] = st.nextToken();
+            name[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(name, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+
+        });
+
+        for(String[] s : name) {
+            System.out.println(s[0] + " " + s[1]);
+        }
+        br.close();
+    }
+}
+
