b548eb232d2759ec875c01929e0fdc605f901bdf
penep0 <>
2025-04-08 19:26:43 +0900

    boj2630

--- codingtest/src/main/java/org/boj2630/Main.java
+++ codingtest/src/main/java/org/boj2630/Main.java
@@ -3,30 +3,51 @@ package org.example;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Boj2630 {
+    public static Integer[][] arr;
+    public static int white = 0;
+    public static int blue = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        String[][] name = new String[n][2];
-        for(int i = 0; i < n; i++) {
+        arr = new Integer[n][n];
+
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            name[i][0] = st.nextToken();
-            name[i][1] = st.nextToken();
-        }
-
-        Arrays.sort(name, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] s1, String[] s2) {
-                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
-
-        });
-
-        for(String[] s : name) {
-            System.out.println(s[0] + " " + s[1]);
         }
+        devideAndConquer(0, 0, n);
+        System.out.println(white + "\n" + blue);
         br.close();
     }
-}
 
+    public static void devideAndConquer(int row, int col, int size) {
+        if(check(row, col, size)) {
+            return;
+        } else {
+            int newSize = size / 2;
+            devideAndConquer(row, col, newSize);
+            devideAndConquer(row, col + newSize, newSize);
+            devideAndConquer(row + newSize, col, newSize);
+            devideAndConquer(row + newSize, col + newSize, newSize);
+        }
+    }
+
+    public static boolean check(int row, int col, int size) {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                if(!arr[row][col].equals(arr[row + i][col + j])) {
+                    return false;
+                }
+            }
+        }
+        if(arr[row][col] == 0) {
+            white++;
+        } else {
+            blue++;
+        }
+        return true;
+    }
+}
