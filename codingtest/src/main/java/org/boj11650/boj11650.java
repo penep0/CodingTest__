723c1e3717b5a364c6e1f340f0e7793512d78c2e
penep0 <>
2025-09-08 13:35:01 +0900

    boj 11650 좌표 정렬하기

---
+++ codingtest/src/main/java/org/boj11650/boj11650.java
@@ -0,0 +1,29 @@
+package org.boj11650;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (a, b) -> {
+            if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
+            return Integer.compare(a[1], b[1]);
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
