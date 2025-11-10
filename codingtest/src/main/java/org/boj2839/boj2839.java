c3057e5b774ae5b266daafbca626343df5022bc0
penep0 <>
2025-11-10 16:51:07 +0900

    boj2839 설탕 배달

---
+++ codingtest/src/main/java/org/boj2839/boj2839.java
@@ -0,0 +1,23 @@
+package org.boj2839;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj2839 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+        while (N >= 0) {
+            if (N % 5 == 0) {
+                result += N / 5;
+                System.out.println(result);
+                return;
+            }
+            N -= 3;
+            result++;
+        }
+
+        System.out.println(-1);
+    }
+}
